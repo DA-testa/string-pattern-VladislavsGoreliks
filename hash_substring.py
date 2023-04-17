15a050a53fc28920347ae3525ee90a97f5041eec
VladislavsGoreliks221RDB245 <125568285+VladislavsGoreliks@users.noreply.github.com>
2023-04-17 09:08:29 +0000

    string-pattern

--- hash_substring.py
+++ hash_substring.py
@@ -1,7 +1,17 @@
 # python3
 
 def read_input():
-    return (input().rstrip(), input().rstrip())
+    source = input().strip().upper()
+    pattern, text = '', ''
+    if source == 'I':
+        pattern = input().strip()
+        text = input().strip()
+    elif source == 'F':
+        with open("tests/06") as f:
+            pattern = f.readline().strip()
+            text = f.readline().strip()
+
+    return pattern, text
 
 
 def print_occurrences(output):
@@ -9,31 +19,29 @@ def print_occurrences(output):
 
 
 def get_occurrences(pattern, text):
-    p = 101
-    m = 10 ** 9 + 7
+    # This function finds the occurrences of the pattern in the text using the Rabin-Karp algorithm
+    # It returns a list of positions where the pattern occurs in the text
+    positions = []
 
-    n = len(text)
-    k = len(pattern)
+    # Get the lengths of the pattern and the text
+    p = len(pattern)
+    t = len(text)
 
-    pattern_hash = 0
-    for i in range(k):
-        pattern_hash = (pattern_hash * p + ord(pattern[i])) % m
+    # Calculate the hash value of the pattern and the first substring of text of length p_len
+    p_hash = sum(ord(pattern[i]) * pow(10, p - 1 - i) for i in range(p))
+    t_hash = sum(ord(text[i]) * pow(10, p - 1 - i) for i in range(p))
 
-    text_hash = 0
-    for i in range(k):
-        text_hash = (text_hash * p + ord(text[i])) % m
+    # Slide the pattern over the text and check for hash match
+    for i in range(t - p + 1):
+        if p_hash == t_hash and pattern == text[i:i+p]:
+            positions.append(i)
+        # If there are more characters in the text, calculate the hash value of the next substring using rolling hash
+        if i < t - p:
+            t_hash = 10 * (t_hash - ord(text[i]) * pow(10, p - 1)) + ord(text[i + p])
 
-    occurrences = []
-    for i in range(n - k + 1):
-        if pattern_hash == text_hash:
-            if pattern == text[i:i + k]:
-                occurrences.append(i)
+    # Return the list of positions where the pattern occurs in the text
+    return positions
 
-        if i < n - k:
-            text_hash = (text_hash - ord(text[i]) * pow(p, k - 1, m)) % m
-            text_hash = (text_hash * p + ord(text[i + k])) % m
-
-    return occurrences
 
 
 if __name__ == '__main__':
