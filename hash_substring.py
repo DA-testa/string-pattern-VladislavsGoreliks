7e9f4f58b59a9e4a3978ec5080b457650e6251bc
VladislavsGoreliks221RDB245 <125568285+VladislavsGoreliks@users.noreply.github.com>
2023-06-01 12:44:01 +0000

    hash_substring

--- hash_substring.py
+++ hash_substring.py
@@ -19,27 +19,23 @@ def print_occurrences(output):
 
 
 def get_occurrences(pattern, text):
-    # This function finds the occurrences of the pattern in the text using the Rabin-Karp algorithm
-    # It returns a list of positions where the pattern occurs in the text
+
     positions = []
 
-    # Get the lengths of the pattern and the text
     p = len(pattern)
     t = len(text)
 
-    # Calculate the hash value of the pattern and the first substring of text of length p_len
     p_hash = sum(ord(pattern[i]) * pow(10, p - 1 - i) for i in range(p))
     t_hash = sum(ord(text[i]) * pow(10, p - 1 - i) for i in range(p))
 
-    # Slide the pattern over the text and check for hash match
     for i in range(t - p + 1):
         if p_hash == t_hash and pattern == text[i:i+p]:
             positions.append(i)
-        # If there are more characters in the text, calculate the hash value of the next substring using rolling hash
+        
         if i < t - p:
             t_hash = 10 * (t_hash - ord(text[i]) * pow(10, p - 1)) + ord(text[i + p])
 
-    # Return the list of positions where the pattern occurs in the text
+
     return positions
 
 
